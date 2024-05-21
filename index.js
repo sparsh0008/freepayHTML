c3fa54c2c9991d552bf81a5e9e213214f6d0822d
sparsh0008 <sparsh.s.k.m@gmail.com>
2024-05-21 15:15:33 +0530

    ..

--- index.js
+++ index.js
@@ -7,14 +7,5 @@ button.addEventListener("click", function () {
     button.classList.add("green");
     button.disabled = true;
     const scannerWindow = window.open('https://qrscaning.netlify.app', 'Scanner', 'width=400,height=400');
-    // Listen for messages from the scanner site
-    window.addEventListener('message', function(event) {
-        // Display the scanned result
-        document.getElementById('result').innerText = `Scanned QR Code: ${event.data}`;
-
-        // Close the scanner window
-        scannerWindow.close();
-    });
-    
     button.innerText = "ON-GOING";
 });
